d12099f7519a13b77928a75dba1b2f5659c41222
Joey Wong <jwong@fubo.tv>
2019-01-27 17:20:01 -0500

    Add todo comments for Form.test.js

--- src/components/Form/__tests__/Form.test.js
+++ src/components/Form/__tests__/Form.test.js
@@ -1,5 +1,13 @@
 describe("Form.js", () => {
   it("", () => {
     //implement me
+    //Part 1 - Basic TDD with RTL
+    //Add two inputs: Movie Title and Genres with their corresponding labels
+    //Add a submit button
+    //Part 2 - Add Mock
+    //1. Add onSubmit functionality to the form
+    //2. Mock the handler to mock an async function called by an external function.
+    //3. The mocked function should be called 1 time.
+    //4. The mocked function should be called with the input value
   });
 });
